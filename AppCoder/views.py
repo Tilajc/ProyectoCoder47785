112f1a6564db9cf17d12629a6732cb05d5c6d900
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-11-22 00:11:13 -0300

    Created my first html in django

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from AppCoder.models import Curso
+from django.http import HttpResponse
+from django.shortcuts import render
+from AppCoder.models import Curso
 
 def crear_curso(request):
     curso = Curso(nombre = 'Python', camada=47785)
     curso.save()
+    contexto = {"curso": curso}
 
-    return HttpResponse(f"su curso es {curso.nombre} y la camada es {curso.camada}")
+    return render(request, 'index.html', contexto)
+
+def show_html(request):
+    curso = Curso.objects.first()
+    contexto = {"curso": curso, "nombre": "Juan Cruz"}
+    return render(request, 'index.html', contexto)
